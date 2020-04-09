81c45dfe2b057cac56e48eddd2845d9ac844e2a8
OI4-Hilscher <oi4development@hilscher.com>
2020-04-09 11:40:52 +0200

    Small adjustment in expert mode text

--- OI4-Cockpit-UI/src/Components/ClickableFooter.jsx
+++ OI4-Cockpit-UI/src/Components/ClickableFooter.jsx
@@ -100,7 +100,7 @@ export class ClickableFooter extends React.Component {
           <TabPanel value={this.state.selectedTab} index={1}>
             <DialogContent style={{ paddingLeft: '10px', paddingRight: '10px' }}>
               <p>In this section, expert configurations can be set by the maintainer.</p>
-              <p>With the expert mode, single assets can be deleted by clicking the bucket icon in the table.</p>
+              <p>With the expert mode, single assets can be deleted from the registry by clicking the bucket icon in the table.</p>
               <p>The conformity test is extended by all available resources (displayed in red). The result of these resources
                 does not influence the general result displayed in the header.
               </p>
