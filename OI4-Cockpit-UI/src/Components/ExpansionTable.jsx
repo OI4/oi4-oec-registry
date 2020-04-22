f5340c88aa44d60641094b5c0864294f9e1e601b
OI4-Hilscher <oi4development@hilscher.com>
2020-04-22 12:30:06 +0200

    UI-Update: Move filtertext TextView to ExpansionPanel Header

--- OI4-Cockpit-UI/src/Components/ExpansionTable.jsx
+++ OI4-Cockpit-UI/src/Components/ExpansionTable.jsx
@@ -111,122 +111,126 @@ class ExpansionTable extends React.Component {
     return (
       <>
         <ExpansionPanel>
-          <ExpansionPanelSummary expandIcon={<ExpandMore />}> {this.state.tableName}: ({Object.keys(this.props.assetLookup).length} entries)</ExpansionPanelSummary>
+          <ExpansionPanelSummary expandIcon={<ExpandMore />}>
+            <div>{this.state.tableName}: ({Object.keys(this.props.assetLookup).length} entries)</div>
+            <TextField
+              id='filterText'
+              value={this.state.filterWord}
+              onChange={this.handleFilterChange.bind(this)}
+              onClick={(ev) => ev.stopPropagation()}
+              onFocus={(ev) => ev.stopPropagation()}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <Search />
+                  </InputAdornment>
+                ),
+              }}
+              placeholder='Filtertext'
+              style={{ marginLeft: 'auto', minWidth: '80px', maxWidth: '200px' }}
+              color='secondary'
+            />
+          </ExpansionPanelSummary>
           <ExpansionPanelDetails className={classes.tableWrap}>
-              <TextField
-                id='filterText'
-                value={this.state.filterWord}
-                onChange={this.handleFilterChange.bind(this)}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position='end'>
-                      <Search />
-                    </InputAdornment>
-                  ),
-                }}
-                placeholder='Filtertext'
-                style={{ minWidth:'80px' }}
-                color='secondary'
-              />
-              <Table className={classes.table}>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Manufacturer</TableCell>
-                    <TableCell>Model</TableCell>
-                    <TableCell>DeviceClass</TableCell>
-                    <TableCell>SerialNumber</TableCell>
-                    <TableCell align="right">Health</TableCell>
-                    <TableCell align="right">Last Message</TableCell>
-                    <TableCell align="right">Conformity</TableCell>
-                    <TableCell align="right">Expand</TableCell>
-                    {this.props.expertMode ? <TableCell align='right'>Delete</TableCell> : null}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {Object.keys(filteredAssets).map((oi4Id, idx) => (
-                    <React.Fragment key={`AssetTable-${oi4Id}-${idx}`}>
-                      {/* {console.log((parseFloat(+(this.props.assetLookup[oi4Id].available)) + 0.5).toString())}
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Manufacturer</TableCell>
+                  <TableCell>Model</TableCell>
+                  <TableCell>DeviceClass</TableCell>
+                  <TableCell>SerialNumber</TableCell>
+                  <TableCell align="right">Health</TableCell>
+                  <TableCell align="right">Last Message</TableCell>
+                  <TableCell align="right">Conformity</TableCell>
+                  <TableCell align="right">Expand</TableCell>
+                  {this.props.expertMode ? <TableCell align='right'>Delete</TableCell> : null}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {Object.keys(filteredAssets).map((oi4Id, idx) => (
+                  <React.Fragment key={`AssetTable-${oi4Id}-${idx}`}>
+                    {/* {console.log((parseFloat(+(this.props.assetLookup[oi4Id].available)) + 0.5).toString())}
                     {console.log(oi4Id)} */}
-                      <TableRow
-                        key={`AssetTable-${oi4Id}-${idx}`}
-                        hoverstyle={{ cursor: "pointer" }}
-                        style={{ opacity: (parseFloat(+(this.props.assetLookup[oi4Id].available)) + 0.5).toString() }}
-                      // FIXME: A bit hacky: we get a bool from availability. Opacity over 1.0 does not hurt, so we add 0.5 to it. false(0) will result in 0.5
-                      // and true(1) will result in 1.5 To parse a bool into a float, we need to make it an integer, this is achieved by + operator
-                      // (nodejs type inference) then parse it to float, add it together and convert it to a string
-                      >
-                        <TableCell component="th" scope="row">{this.getResourceObject(oi4Id, 'mam').Manufacturer.Text}</TableCell>
-                        <TableCell component="th" scope="row">{this.getResourceObject(oi4Id, 'mam').Model.Text}</TableCell>
-                        <TableCell component="th" scope="row">{this.getResourceObject(oi4Id, 'mam').DeviceClass}</TableCell>
-                        <TableCell component="th" scope="row">{this.getResourceObject(oi4Id, 'mam').SerialNumber}</TableCell>
-                        <TableCell align="right">{this.displayNamurHealth(this.getHealth(oi4Id, 'application'))}</TableCell>
-                        <TableCell align="right">{this.props.assetLookup[oi4Id].lastMessage}</TableCell>
-                        <TableCell align="right">
-                          <Typography variant='h6'><span role="img" aria-label="check">{this.displayConformityHeader(oi4Id)}</span></Typography>
-                        </TableCell>
-                        <TableCell align="right">
-                          <IconButton
-                            size='small'
-                            color='default'
-                            onClick={() => {
-                              const expandedLookupCopy = JSON.parse(JSON.stringify(this.state.expandedLookup));
-                              if (oi4Id in expandedLookupCopy) {
-                                expandedLookupCopy[oi4Id] = !(expandedLookupCopy[oi4Id]);
-                              } else {
-                                expandedLookupCopy[oi4Id] = true;
-                              }
-                              this.setState({ expandedLookup: expandedLookupCopy });
-                            }}
-                          >
-                            {this.displayTableExpansion(oi4Id)}
-                          </IconButton>
-                        </TableCell>
-                        {this.props.expertMode ? <TableCell align="right">
-                          <IconButton
-                            size='small'
-                            color='default'
-                            onClick={(evt) => {
-                              evt.preventDefault();
-                              this.props.clearAsset(oi4Id);
-                            }}
-                          >
-                            <Delete />
-                          </IconButton>
-                        </TableCell> : null}
-                      </TableRow>
-                      <TableRow key={`AssetTableDetail-${oi4Id}-${idx}`}>
-                        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
-                          <Collapse
-                            className={classes.tableInside}
-                            in={this.state.expandedLookup[oi4Id]}
-                            timeout='auto'
-                            unmountOnExit
-                          >
+                    <TableRow
+                      key={`AssetTable-${oi4Id}-${idx}`}
+                      hoverstyle={{ cursor: "pointer" }}
+                      style={{ opacity: (parseFloat(+(this.props.assetLookup[oi4Id].available)) + 0.5).toString() }}
+                    // FIXME: A bit hacky: we get a bool from availability. Opacity over 1.0 does not hurt, so we add 0.5 to it. false(0) will result in 0.5
+                    // and true(1) will result in 1.5 To parse a bool into a float, we need to make it an integer, this is achieved by + operator
+                    // (nodejs type inference) then parse it to float, add it together and convert it to a string
+                    >
+                      <TableCell component="th" scope="row">{this.getResourceObject(oi4Id, 'mam').Manufacturer.Text}</TableCell>
+                      <TableCell component="th" scope="row">{this.getResourceObject(oi4Id, 'mam').Model.Text}</TableCell>
+                      <TableCell component="th" scope="row">{this.getResourceObject(oi4Id, 'mam').DeviceClass}</TableCell>
+                      <TableCell component="th" scope="row">{this.getResourceObject(oi4Id, 'mam').SerialNumber}</TableCell>
+                      <TableCell align="right">{this.displayNamurHealth(this.getHealth(oi4Id, 'application'))}</TableCell>
+                      <TableCell align="right">{this.props.assetLookup[oi4Id].lastMessage}</TableCell>
+                      <TableCell align="right">
+                        <Typography variant='h6'><span role="img" aria-label="check">{this.displayConformityHeader(oi4Id)}</span></Typography>
+                      </TableCell>
+                      <TableCell align="right">
+                        <IconButton
+                          size='small'
+                          color='default'
+                          onClick={() => {
+                            const expandedLookupCopy = JSON.parse(JSON.stringify(this.state.expandedLookup));
+                            if (oi4Id in expandedLookupCopy) {
+                              expandedLookupCopy[oi4Id] = !(expandedLookupCopy[oi4Id]);
+                            } else {
+                              expandedLookupCopy[oi4Id] = true;
+                            }
+                            this.setState({ expandedLookup: expandedLookupCopy });
+                          }}
+                        >
+                          {this.displayTableExpansion(oi4Id)}
+                        </IconButton>
+                      </TableCell>
+                      {this.props.expertMode ? <TableCell align="right">
+                        <IconButton
+                          size='small'
+                          color='default'
+                          onClick={(evt) => {
+                            evt.preventDefault();
+                            this.props.clearAsset(oi4Id);
+                          }}
+                        >
+                          <Delete />
+                        </IconButton>
+                      </TableCell> : null}
+                    </TableRow>
+                    <TableRow key={`AssetTableDetail-${oi4Id}-${idx}`}>
+                      <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
+                        <Collapse
+                          className={classes.tableInside}
+                          in={this.state.expandedLookup[oi4Id]}
+                          timeout='auto'
+                          unmountOnExit
+                        >
+                          <div>
+                            <ExpansionTableDetail
+                              asset={this.props.assetLookup[oi4Id]}
+                              conformityLookup={this.props.conformityLookup}
+                              expertMode={this.props.expertMode}
+                              oi4Id={oi4Id}
+                              assetLookup={this.props.assetLookup}
+                              updateConformity={this.updateConformity.bind(this)}
+                              getResourceObject={this.getResourceObject.bind(this)}
+                              lookupType={this.props.lookupType}
+                              fontColor={this.props.fontColor}
+                              updatingConformity={this.props.updatingConformity}
+                            />
                             <div>
-                              <ExpansionTableDetail
-                                asset={this.props.assetLookup[oi4Id]}
-                                conformityLookup={this.props.conformityLookup}
-                                expertMode={this.props.expertMode}
-                                oi4Id={oi4Id}
-                                assetLookup={this.props.assetLookup}
-                                updateConformity={this.updateConformity.bind(this)}
-                                getResourceObject={this.getResourceObject.bind(this)}
-                                lookupType={this.props.lookupType}
-                                fontColor={this.props.fontColor}
-                                updatingConformity={this.props.updatingConformity}
-                              />
-                              <div>
-                                <h3>{`Last ${this.props.assetLookup[oi4Id].eventList.length} Events:`}</h3>
-                                {this.displayLocalEvents(this.props.assetLookup[oi4Id].eventList)}
-                              </div>
+                              <h3>{`Last ${this.props.assetLookup[oi4Id].eventList.length} Events:`}</h3>
+                              {this.displayLocalEvents(this.props.assetLookup[oi4Id].eventList)}
                             </div>
-                          </Collapse>
-                        </TableCell>
-                      </TableRow>
-                    </React.Fragment>
-                  ))}
-                </TableBody>
-              </Table>
+                          </div>
+                        </Collapse>
+                      </TableCell>
+                    </TableRow>
+                  </React.Fragment>
+                ))}
+              </TableBody>
+            </Table>
           </ExpansionPanelDetails>
         </ExpansionPanel>
       </>
