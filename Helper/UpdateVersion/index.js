d79740d297d7c073689e8f40e9b6de86f199e651
OI4-Hilscher <oi4development@hilscher.com>
2020-04-28 12:14:08 +0200

    Add Helper to update the version of all package.json files and mam.json file from the source package.json

---
+++ Helper/UpdateVersion/index.js
@@ -0,0 +1,30 @@
+const rootDir = '.';
+const masterConfig = require(`../../package.json`);
+console.log(masterConfig);
+const fs = require('fs');
+
+let currentDir = `${rootDir}/OI4-Cockpit-UI/package.json`;
+let packageJsonLoc = readJSON(currentDir);
+packageJsonLoc.version = masterConfig.version;
+fs.writeFileSync(currentDir, `${JSON.stringify(packageJsonLoc, null, 2)}\n`);
+
+currentDir = `${rootDir}/OI4-Local-UI/package.json`
+packageJsonLoc = readJSON(currentDir);
+packageJsonLoc.version = masterConfig.version;
+fs.writeFileSync(currentDir, `${JSON.stringify(packageJsonLoc, null, 2)}\n`);
+
+currentDir = `${rootDir}/OI4-Service/package.json`
+packageJsonLoc = readJSON(currentDir);
+packageJsonLoc.version = masterConfig.version;
+fs.writeFileSync(currentDir, `${JSON.stringify(packageJsonLoc, null, 2)}\n`);
+
+currentDir = `${rootDir}/OI4-Service/src/Config/masterAssetModel.json`
+packageJsonLoc = readJSON(currentDir);
+packageJsonLoc.SoftwareRevision = masterConfig.version;
+fs.writeFileSync(currentDir, `${JSON.stringify(packageJsonLoc, null, 2)}`);
+
+function readJSON(path) {
+    const json = JSON.parse(fs.readFileSync(path));
+    console.log(`Read JSON from Path: ${path} - ${JSON.stringify(json.version)}`);
+    return json;
+}
