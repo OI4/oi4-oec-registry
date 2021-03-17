ac4ae676a4473ed562154bd3b9c068d6550a091e
OI4-Hilscher <55870966+OI4-Hilscher@users.noreply.github.com>
2021-03-17 10:04:31 +0100

    Small name change

--- OI4-Local-UI/src/Components/ExpansionTableDetail.jsx
+++ OI4-Local-UI/src/Components/ExpansionTableDetail.jsx
@@ -234,7 +234,7 @@ class ExpansionTableDetail extends React.Component {
     } else {
       if (typeof conformityObject === 'object' && conformityObject !== null) {
         return <div>
-          <b>oi4 identifier: </b>{conformityObject.oi4Id}
+          <b>oi4Identifier: </b>{conformityObject.oi4Id}
           {
             Object.keys(conformityObject.resource).map((resources) => {
               let resourceColor = this.props.fontColor;
