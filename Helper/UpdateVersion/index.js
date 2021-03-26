0eca22c9d586c0a42cc02efb6ee92eb9f0818382
OI4-Hilscher <55870966+OI4-Hilscher@users.noreply.github.com>
2021-03-26 12:31:51 +0100

    Update Version helper: Remove cockpit remnants

--- Helper/UpdateVersion/index.js
+++ Helper/UpdateVersion/index.js
@@ -3,13 +3,8 @@ const masterConfig = require(`../../package.json`);
 console.log(masterConfig);
 const fs = require('fs');
 
-let currentDir = `${rootDir}/OI4-Cockpit-UI/package.json`;
+let currentDir = `${rootDir}/OI4-Local-UI/package.json`
 let packageJsonLoc = readJSON(currentDir);
-packageJsonLoc.version = masterConfig.version;
-fs.writeFileSync(currentDir, `${JSON.stringify(packageJsonLoc, null, 2)}\n`);
-
-currentDir = `${rootDir}/OI4-Local-UI/package.json`
-packageJsonLoc = readJSON(currentDir);
 packageJsonLoc.version = masterConfig.version;
 fs.writeFileSync(currentDir, `${JSON.stringify(packageJsonLoc, null, 2)}\n`);
 
