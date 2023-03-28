ba5fd320705e434a404c65007ac8a52154e24059
Thomas Weinschenk <thomas.weinschenk@endress.com>
2023-03-28 11:12:46 +0200

    Version bumped

--- copy_service.sh
+++ copy_service.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-rm rf ./node_modules/@oi4/oi4-oec-dnp-encoding
-rm rf ./node_modules/@oi4/oi4-oec-json-schemas
-rm rf ./node_modules/@oi4/oi4-oec-service-conformity-validator
-rm rf ./node_modules/@oi4/oi4-oec-service-logger
-rm rf ./node_modules/@oi4/oi4-oec-service-model
-rm rf ./node_modules/@oi4/oi4-oec-service-node
+rm -rf ./node_modules/@oi4/oi4-oec-dnp-encoding
+rm -rf ./node_modules/@oi4/oi4-oec-json-schemas
+rm -rf ./node_modules/@oi4/oi4-oec-service-conformity-validator
+rm -rf ./node_modules/@oi4/oi4-oec-service-logger
+rm -rf ./node_modules/@oi4/oi4-oec-service-model
+rm -rf ./node_modules/@oi4/oi4-oec-service-node
 
 cp -r $oec_service_root/node_modules/@oi4/oi4-oec-dnp-encoding ./node_modules/@oi4/oi4-oec-dnp-encoding
 cp -r $oec_service_root/node_modules/@oi4/oi4-oec-json-schemas ./node_modules/@oi4/oi4-oec-json-schemas
