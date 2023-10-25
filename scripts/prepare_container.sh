8453d77d63c53d5c10878b3ab95f6b2f570c9b7c
Thomas Weinschenk <thomas.weinschenk@endress.com>
2023-10-25 08:36:22 +0200

    Fixed container preparation

--- scripts/prepare_container.sh
+++ scripts/prepare_container.sh
@@ -3,6 +3,7 @@ rm -rf ./build
 
 mkdir -p ./build/container/packages/oi4-local-ui
 mkdir -p ./build/container/packages/oi4-registry-service
+mkdir -p ./build/container/packages/oi4-registry-service/scripts
 
 echo "***************************"
 echo "**  Init parent package  **"
@@ -15,6 +16,7 @@ cd ../..
 echo "************************************"
 echo "**  Install oi4-registry-service  **"
 echo "************************************"
+
 cp ./scripts/deploy-config-ui.sh ./build/container/packages/oi4-registry-service/scripts/deploy-config-ui.sh
 cp ./scripts/entrypoint.sh ./build/container/packages/oi4-registry-service/scripts/entrypoint.sh
 cp ./packages/oi4-registry-service/package.json ./build/container/packages/oi4-registry-service/package.json
@@ -22,6 +24,8 @@ cp ./packages/oi4-registry-service/.npmrc ./build/container/packages/oi4-registr
 cp -r ./packages/oi4-registry-service/dist/ ./build/container/packages/oi4-registry-service/src
 cp -r ./packages/oi4-registry-service/public/ ./build/container/packages/oi4-registry-service/public
 cd ./build/container/packages/oi4-registry-service || exit
+echo "build:"
+ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
 yarn install --production
 cd ../../../..
 
@@ -31,3 +35,7 @@ echo "**  Install oi4-local-ui  **"
 echo "****************************"
 cp ./packages/oi4-local-ui/package.json ./build/container/packages/oi4-local-ui/package.json
 cp -r ./packages/oi4-local-ui/build ./build/container/packages/oi4-local-ui/
+
+cd ./packages/oi4-registry-service/dist/
+echo "dist:"
+ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
